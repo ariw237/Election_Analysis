6e590a767015c7924c7a14458be8a4dc8d83767f
ariw237 <ariw@uoregon.edu>
2021-10-12 17:58:03 -0700

    Added modified PyPoll file

--- PyPoll_new.py
+++ PyPoll_new.py
@@ -37,22 +37,36 @@ with open(file_to_load) as election_data:
         #Count total number of votes
         total_votes += 1
         candidate_votes[candidate_name] += 1
-print(total_votes)
-print(candidate_list)
-print(candidate_votes)
-for candidate in candidate_list:
-    votes = (candidate_votes[candidate])
-    vote_percentage = votes/(total_votes) * 100
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate
-    print(f"{candidate}: received {vote_percentage:.2f}% of the vote.")
+#print(total_votes)
+#print(candidate_list)
+#print(candidate_votes)
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
     
-winning_candidate_summary = (
+    for candidate in candidate_list:
+        votes = (candidate_votes[candidate])
+        vote_percentage = votes/(total_votes) * 100
+        candidate_result = (f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_result)
+        txt_file.write(candidate_result)
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate
+        #print(f"{candidate}: received {vote_percentage:.2f}% of the vote.")
+    winning_candidate_summary = (
     f"-------------------------\n"
     f"Winner: {winning_candidate}\n"
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
-print(winning_candidate_summary) 
+    print(winning_candidate_summary) 
+    txt_file.write(winning_candidate_summary)
+
+    
